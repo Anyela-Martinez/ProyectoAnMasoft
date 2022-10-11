edaacb099e1c2bc78d67cf3d4e8a70f818f49f81
Ivan-Castro30 <92544722+Ivan-Cast@users.noreply.github.com>
2022-10-11 16:25:18 -0500

    Update models.py

--- docentes/models.py
+++ docentes/models.py
@@ -1,3 +1,7 @@
+from tabnanny import verbose
 from django.db import models
 
+from usuarios.models import Usuario
+
 # Create your models here.
+usuario=models.ForeignKey(Usuario, on_delete=models.CASCADE,verbose_name='Usuario')
