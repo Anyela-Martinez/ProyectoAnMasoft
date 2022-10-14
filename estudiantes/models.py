c29fa3095fb002c4a7188a35aa3fcb0d6e4259cf
Jessica666sierra <113215443+Jessica666sierra@users.noreply.github.com>
2022-10-13 20:13:46 -0500

    Creacion de modulos
    se creo modulo grado asignatura estudiante

--- estudiantes/models.py
+++ estudiantes/models.py
@@ -1,3 +1,70 @@
 from django.db import models
+from socket import TIPC_NODE_SCOPE
+from statistics import correlation
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from usuarios.models import Usuario
+
+# Create your models here.
+class Estudiante(models.Model):
+    class TipoDoc(models.Model):
+        RC='RC', _('Registro Civil')
+        TI='TI', _('Tarjeta de Identidad')
+        CC='CC', _('Cédula de Ciudadanía')
+        NES='NES', _('Número de Secretaria')
+        PP='PP', _('Pasaporte')
+        PV='PV', _('Pasaporte Venezolano')
+    TipoDoc=models.CharField(max_length=6,choices=TipoDoc.choices, default=TipoDoc.RC, verbose_name="Tipo de Documento") 
+
+    numDoc=models.CharField(max_length=60, verbose_name="Número de Documento") 
+
+    nombres=models.CharField(max_length=60, verbose_name="Nombres")
+
+    apellidos=models.CharField(max_length=60, verbose_name="Apellidos")
+
+    class Jornada(models.Model):
+        JM='MAÑANA', _('Jornada Mañana')
+        JT='TARDE', _('Jornada Tarde')
+    Jornada=models.CharField(max_length=2,choices=Jornada.choices, default=Jornada.JM, verbose_name="Jornada")
+    telefono=models.CharField(max_length=20, verbose_name="Teléfono ")  
+
+    
+    direccion=models.CharField(max_length=40, verbose_name="Dirección") 
+
+    correo=models.CharField(max_length=40, verbose_name="Correo Electrónico")
+     
+    class Genero(models.Model):
+        M='M', _('Masculino')
+        F='F', _('Femenino')
+        I='I', _('Indefinido')
+    Genero=models.CharField(max_length=3,choices=Genero.choices, default=Genero.M, verbose_name="Género")
+    
+    class Estado(models.Model):
+        ACTIVO='1', _('Activo')
+        INACTIVO='0', _('Inactivo')
+    Estado=models.CharField(max_length=3,choices=Estado.choices, default=Estado.ACTIVO, verbose_name="Estado")
+    
+    class TipoDocAcu(models.Model):
+            CC='CC', _('Cédula de Ciudadanía')
+            CE='CE', _('Cédula de Extranjería')
+            PP='PP', _('Pasaporte')
+    TipoDocAcu=models.CharField(max_length=3,choices=TipoDocAcu.choices, default=TipoDocAcu.CC, verbose_name="Tipo de Documento del Acudiente") 
+
+    numDocAcu=models.CharField(max_length=60, verbose_name="Número de Documento del Acudiente")
+
+    nombresAcu=models.CharField(max_length=60, verbose_name="Nombres del Acudiente")
+
+    apellidosAcu=models.CharField(max_length=60, verbose_name="Apellidos del Acudiente")
+
+    telefonoAcu=models.CharField(max_length=20, verbose_name="Teléfono del Acudiente") 
+
+    correoAcu=models.CharField(max_length=40, verbose_name="Correo Electrónico del Acudiente")
+    
+    usuario=models.ForeignKey(Usuario, on_delete=models.CASCADE,verbose_name='Usuario')
+    
+    curso=models.ForeignKey(Curso, on_delete=models.CASCADE,verbose_name='Curso')
+
+    
 
 # Create your models here.
--- grado/models.py
+++ grado/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+from usuarios.models import Usuario
+
 # Create your models here.
+class Grado(models.Model):
+    usuario=models.ForeignKey(Usuario, on_delete=models.CASCADE,verbose_name='Usuario')
+    nombreGrado=models.CharField(max_length=20, verbose_name="Nombre de grado")
+    numGrado=models.CharField(max_length=20, verbose_name="Numero de grado")
+    
